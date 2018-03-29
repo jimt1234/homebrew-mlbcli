3a15b00ec1360c300a742212b20600aecdfe37aa
jimt1234 <jimt1234@gmail.com>
2018-03-29 00:23:54 -0700

    update formula

--- mlbcli.rb
+++ mlbcli.rb
@@ -1,4 +1,5 @@
 HOMEBREW_MLBCLI_VERSION = "0.1.0".freeze
+HOMEBREW_MLBCLI_NAME = "mlbcli-#{HOMEBREW_MLBCLI_VERSION}-darwin-amd64".freeze
 HOMEBREW_MLBCLI_SHA = "b5deb09193497d4e87406bcfef1f8ea63ff8eecc38aa54fa3d2f8ef4810abc6e".freeze
 
 class Mlbcli < Formula
