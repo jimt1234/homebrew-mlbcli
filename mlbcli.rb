a565a7a2217325214031ca19abb809bd4e8690e4
jimt1234 <jimt1234@gmail.com>
2018-03-29 00:36:49 -0700

    update formula desc

--- mlbcli.rb
+++ mlbcli.rb
@@ -3,7 +3,7 @@ HOMEBREW_MLBCLI_NAME = "mlbcli-#{HOMEBREW_MLBCLI_VERSION}-darwin-amd64".freeze
 HOMEBREW_MLBCLI_SHA = "b5deb09193497d4e87406bcfef1f8ea63ff8eecc38aa54fa3d2f8ef4810abc6e".freeze
 
 class Mlbcli < Formula
-  desc "Command-line tool to retrieve MLB scores"
+  desc "Command-line tool to display MLB scoreboards"
   homepage "https://github.com/jimt1234/mlbcli"
   url "#{homepage}/releases/download/v#{HOMEBREW_MLBCLI_VERSION}/mlbcli-#{HOMEBREW_MLBCLI_VERSION}-darwin-amd64.zip"
   version HOMEBREW_MLBCLI_VERSION
