9310124a6cb2dedf98c55fc7c795827020961a23
jimt1234 <jimt1234@gmail.com>
2018-03-29 00:20:17 -0700

    update formula

--- mlbcli.rb
+++ mlbcli.rb
@@ -1,18 +1,19 @@
-require 'formula'
+HOMEBREW_MLBCLI_VERSION = "0.1.0".freeze
+HOMEBREW_MLBCLI_SHA = "b5deb09193497d4e87406bcfef1f8ea63ff8eecc38aa54fa3d2f8ef4810abc6e".freeze
 
 class Mlbcli < Formula
-  desc 'Command-line tool to retrieve MLB scores'
-  homepage 'https://github.com/jimt1234/mlbcli'
-  version '0.1.0'
-  url 'https://github.com/jimt1234/mlbcli/releases/download/v0.1.0/mlbcli-0.1.0-darwin-amd64.zip'
-  sha256 'b5deb09193497d4e87406bcfef1f8ea63ff8eecc38aa54fa3d2f8ef4810abc6e'
+  desc "Command-line tool to retrieve MLB scores"
+  homepage "https://github.com/jimt1234/mlbcli"
+  url "#{homepage}/releases/download/v#{HOMEBREW_MLBCLI_VERSION}/mlbcli-#{HOMEBREW_MLBCLI_VERSION}-darwin-amd64.zip"
+  version HOMEBREW_MLBCLI_VERSION
+  sha256 HOMEBREW_MLBCLI_SHA
 
   def install
-    bin.install Dir['mlbcli']
+    mv "#{buildpath}/#{HOMEBREW_MLBCLI_NAME}", "#{buildpath}/mlbcli"
+    bin.install "mlbcli"
   end
 
   test do
     `mlbcli -h`
   end
-
 end
